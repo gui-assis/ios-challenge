ff9f4303d5595dbf25a4d3d7fec773c1c36296b5
gui-assis <Guilherme@Mac.local>
2015-06-16 23:02:40 -0300

    initial commit with cocoapods setup

---
+++ ios-challenge/ios-challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-challenge
+//
+//  Created by Guilherme Augusto on 16/06/15.
+//  Copyright (c) 2015 helabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
